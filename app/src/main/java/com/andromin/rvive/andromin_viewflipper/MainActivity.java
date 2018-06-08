0f12dad3bcfdfbba3e0ca13ae175f7862faa395e
Vivek <viveknarayanan15082@cse.ssn.edu.in>
2018-06-08 11:19:19 +0530

    " Added comments "

--- app/src/main/java/com/andromin/rvive/andromin_viewflipper/MainActivity.java
+++ app/src/main/java/com/andromin/rvive/andromin_viewflipper/MainActivity.java
@@ -19,13 +19,26 @@ public class MainActivity extends AppCompatActivity {
 
     public void viewflip()
     {
+        // -------View Flipper
+        //1 - Create the animations, we will load the animations from
+        //    the already made animations in the android.R.anim folder
+        //    custom animations can be added into to res/anim folder
         Animation anim_in = AnimationUtils.loadAnimation(this,android.R.anim.slide_in_left);
         Animation anim_out = AnimationUtils.loadAnimation(this,android.R.anim.slide_out_right);
 
-        ViewFlipper vf = (ViewFlipper)findViewById(R.id.vf);
+
+        //2 - Find the fipper in the XML
+        ViewFlipper vf = findViewById(R.id.vf);
+
+        //3 - Assisgn the animations
         vf.setInAnimation(anim_in);
         vf.setOutAnimation(anim_out);
+
+        //4 - Set the flipping interval, that is the time between flips
         vf.setFlipInterval(3000);
+
+        //5 - Start FLipping - optional here, can also be
+        //    called on click for click to flip
         vf.startFlipping();
     }
 
